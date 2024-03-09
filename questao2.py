2cf7ddc4374a8059ef789e26e25b07ad6cc24345
Lívia Mariana Lopes <138078091+LiviaMarianaLopes@users.noreply.github.com>
2024-03-09 15:52:09 -0300

    Create questao2.py

---
+++ questao2.py
@@ -0,0 +1,15 @@
+def pertence_fibonacci(n: str):
+    """Função que verifica se um número pertence a sequência de Fibonacci"""
+    fib_sequence = [0, 1]
+    while fib_sequence[-1] < n:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    if n in fib_sequence:
+        return f"O número {n} pertence à sequência de Fibonacci."
+    else:
+        return f"O número {n} não pertence à sequência de Fibonacci."
+
+
+numero = int(input("Programa para verificar se um número está na sequência de Fibonacci"
+                   "\nNúmero: "))
+
+print(pertence_fibonacci(numero))
